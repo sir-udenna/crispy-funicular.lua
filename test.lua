64ed6d124d93c23874c84a0d5b674eab8e5dd018
Udenna Nebeolisa <66294750+sir-udenna@users.noreply.github.com>
2021-05-30 15:02:09 -0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -9,7 +9,30 @@
 
 print('ran')
 
+-- RunService = game:GetService('RunService')
+-- Players = game:GetService('Players')
+-- Player = Players.LocalPlayer
+-- Mouse = Player:GetMouse()
+-- Char = Player.Character
+-- if not Char then
+-- 	Player.CharacterAdded:Wait()
+-- 	Char = Player.Character
+-- end
+
+-- RootPart = Char:WaitForChild('HumanoidRootPart')
+
+-- RunService.Stepped:connect(function()
+	
+-- 	local MousePos = Mouse.Hit.p
+	
+-- 	local lookToPosVector = Vector3.new(MousePos.X,RootPart.CFrame.Y,MousePos.Z)
+	
+-- 	RootPart.CFrame = CFrame.new(RootPart.CFrame.p,lookToPosVector)
+	
+-- end)
+
 RunService = game:GetService('RunService')
+TweenService = game:GetService("TweenService")
 Players = game:GetService('Players')
 Player = Players.LocalPlayer
 Mouse = Player:GetMouse()
@@ -25,8 +48,16 @@ RunService.Stepped:connect(function()
 	
 	local MousePos = Mouse.Hit.p
 	
-	local lookToPosVector = Vector3.new(MousePos.X,RootPart.CFrame.Y,MousePos.Z)
+	local lookVector = Vector3.new(MousePos.X,RootPart.CFrame.Y,MousePos.Z)
 	
-	RootPart.CFrame = CFrame.new(RootPart.CFrame.p,lookToPosVector)
+	--RootPart.CFrame = CFrame.new(RootPart.CFrame.p,lookVector)
+	local goal = {}
+	goal.CFrame = CFrame.new(RootPart.CFrame.p,lookVector)
+ 
+	local tweenInfo = TweenInfo.new(1)
+ 
+	local tween = TweenService:Create(RootPart, tweenInfo, goal)
+ 
+	tween:Play()
 	
 end)
