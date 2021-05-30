a5a413c6a5085205538d4de889a086482fa0d20e
Udenna Nebeolisa <66294750+sir-udenna@users.noreply.github.com>
2021-05-30 14:48:24 -0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,8 +1,1 @@
-local UserInputService = game:GetService("UserInputService")
-local Mouse = game:GetService("Players").LocalPlayer:GetMouse()
-local Root = script.Parent:WaitForChild("HumanoidRootPart")
-
-Mouse.Move:Connect(function()
-	if not UserInputService:IsKeyDown(Enum.KeyCode.Q) then return end
-	Root.CFrame = CFrame.new(Root.Position, Root.Position + Mouse.Hit.LookVector)
-end)
+print('testomg')
